1a581b9da13176c3fb52e6931a74e9e900edc358
Encrypted <thoughts.encrypted@gmail.com>
2021-10-23 21:19:23 -0500

    fixed corner rounding to match windows 11 per microsoft instructions - https://docs.microsoft.com/en-us/windows/apps/desktop/modernize/apply-rounded-corners

--- TwitchVodChatHistoryTool/TwitchVodChatHistoryTool/MainWindow.xaml.cs
+++ TwitchVodChatHistoryTool/TwitchVodChatHistoryTool/MainWindow.xaml.cs
@@ -3,8 +3,10 @@ using ChatHistory.Models;
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 
 namespace TwitchVodChatHistoryTool
 {
@@ -14,10 +16,36 @@ namespace TwitchVodChatHistoryTool
     public partial class MainWindow
     {
         public ChatToolContext context = new();
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+        // The DWM_WINDOW_CORNER_PREFERENCE enum for DwmSetWindowAttribute's third parameter, which tells the function
+        // what value of the enum to set.
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+
+        // Import dwmapi.dll and define DwmSetWindowAttribute in C# corresponding to the native function.
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern long DwmSetWindowAttribute(IntPtr hwnd,
+                                                         DWMWINDOWATTRIBUTE attribute,
+                                                         ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
+                                                         uint cbAttribute);
         public MainWindow()
         {
             InitializeComponent();
             DataContext = context;
+
+            IntPtr hWnd = new WindowInteropHelper(GetWindow(this)).EnsureHandle();
+            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+            DwmSetWindowAttribute(hWnd, attribute, ref preference, sizeof(uint));
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
